93233435bdb0360e81364273bf49300f4235b975
jjunyjjuny <jjuny546@gmail.com>
2021-07-26 21:19:51 +0900

    Init: webpack, typescript 설정

---
+++ client/webpack.config.prod.js
@@ -0,0 +1,52 @@
+require('dotenv/config');
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+	mode: 'production',
+	entry: './src/index.ts',
+	output: {
+		filename: 'bundle.js',
+		path: path.resolve(__dirname, 'dist'),
+	},
+	resolve: {
+		extensions: ['.ts', '.js', '.json'],
+	},
+	module: {
+		rules: [
+			{
+				test: /\.ts?$/,
+				use: 'ts-loader',
+				exclude: /node_modules/,
+			},
+			{
+				test: /\.s[ac]ss$/,
+				use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+				exclude: /node_modules/,
+			},
+			{
+				test: /\.(png|jp(e*)g)$/,
+				loader: 'url-loader',
+				options: {
+					limit: 8000,
+					name: 'images/[hash]-[name].[ext]',
+				},
+			},
+			{
+				test: /\.svg$/,
+				loader: 'file-loader',
+			},
+		],
+	},
+	plugins: [
+		new MiniCssExtractPlugin({
+			filename: 'style.css',
+		}),
+		new HtmlWebpackPlugin({
+			template: './src/index.html',
+		}),
+		new webpack.EnvironmentPlugin([]),
+	],
+};
