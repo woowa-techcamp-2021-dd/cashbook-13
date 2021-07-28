f183876737f0ae45f2f563af64272cba5479ca76
jjunyjjuny <jjuny546@gmail.com>
2021-07-29 00:11:28 +0900

    chore: client dummy

--- client/src/index.ts
+++ client/src/index.ts
@@ -1,3 +1,16 @@
 import './style.scss';
 
 console.log('데모입니다');
+
+const $test = document.getElementById('test');
+
+$test?.addEventListener('click', async () => {
+	// const res = await fetch('http://localhost:4000/api/record?userID=1').then(
+	// 	(res) => {
+	// 		if (!res.ok) return;
+	// 		return res.json();
+	// 	}
+	// );
+	// console.log(res);
+	const res = await fetch('http://localhost:4000/api/record?userID=1');
+});
