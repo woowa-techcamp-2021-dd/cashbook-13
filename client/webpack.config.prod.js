52a91bbf28265926d5d65558367e7343d5688d3a
jjunyjjuny <jjuny546@gmail.com>
2021-07-31 18:47:38 +0900

    chore: 우아한 jsx 추가, 그를 위한 babel 설정 변경

--- client/webpack.config.prod.js
+++ client/webpack.config.prod.js
@@ -6,7 +6,7 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
 	mode: 'production',
-	entry: './src/index.ts',
+	entry: './src/index.js',
 	output: {
 		filename: 'bundle.js',
 		path: path.resolve(__dirname, 'dist'),
