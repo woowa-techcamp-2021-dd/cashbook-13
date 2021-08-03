5053ec48a0d3f75c636489ebef28cb2c618a9425
jjunyjjuny <jjuny546@gmail.com>
2021-08-04 01:47:38 +0900

    Chore: authRouter - authController 연결, github OAuth  경로 수정

--- server/src/routes/auth/index.ts
+++ server/src/routes/auth/index.ts
@@ -1,15 +1,17 @@
 import { Router } from 'express';
 import passport from 'passport';
+import authController from '../../controllers/auth.controller';
 import gitPassport from '../../middlewares/git-oauth.middleware';
 
 const authRouter = Router();
-
 gitPassport();
 
-authRouter.get('/login/github', passport.authenticate('github'));
+authRouter.post('/signup', authController.signUp);
+authRouter.post('/signin', authController.signIn);
+authRouter.get('/signin/github', passport.authenticate('github'));
 
 authRouter.get(
-	'/login/github/callback',
+	'/signin/github/callback',
 	passport.authenticate('github', {
 		failureRedirect: '/fail',
 		successRedirect: 'http://localhost:8080',
