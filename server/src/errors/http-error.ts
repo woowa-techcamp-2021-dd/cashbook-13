445ff606ce6cecf0cad24ca6c9e58d2b54e5e5c6
jjunyjjuny <jjuny546@gmail.com>
2021-08-04 01:44:52 +0900

    Feat: HTTPError class

---
+++ server/src/errors/http-error.ts
@@ -0,0 +1,10 @@
+class HTTPError extends Error {
+	status!: number;
+
+	constructor(status: number, message: string) {
+		super(message);
+		this.status = status;
+	}
+}
+
+export default HTTPError;
