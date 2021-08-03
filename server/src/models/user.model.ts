175e5104b5fe8e2a843293929d2320c7add787ba
jjunyjjuny <jjuny546@gmail.com>
2021-08-04 01:45:38 +0900

    Fix: githubID not null -> null로 변경

--- server/src/models/user.model.ts
+++ server/src/models/user.model.ts
@@ -1,16 +1,16 @@
-import { Sequelize, DataTypes, Model } from 'sequelize';
+import { Sequelize, DataType, Model } from 'sequelize-typescript';
 import { sequelize } from '.';
 
 export interface UserAttributes {
 	name: string;
-	githubID: string;
+	githubID?: string;
 	createdAt?: Date;
 	updatedAt?: Date;
 }
 
 export default class User extends Model<UserAttributes> {
 	public readonly id!: number;
-	public githubID!: string;
+	public githubID?: string;
 	public name!: string;
 
 	public createdAt!: Date;
@@ -20,11 +20,11 @@ export default class User extends Model<UserAttributes> {
 User.init(
 	{
 		githubID: {
-			type: DataTypes.STRING(45),
-			allowNull: false,
+			type: DataType.STRING(45),
+			allowNull: true,
 		},
 		name: {
-			type: DataTypes.STRING(45),
+			type: DataType.STRING(45),
 			allowNull: false,
 		},
 		createdAt: {
