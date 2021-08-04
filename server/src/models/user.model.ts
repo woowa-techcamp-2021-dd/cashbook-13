ba38f907a34300fa5cea2068e9a114c79061c93d
jjunyjjuny <jjuny546@gmail.com>
2021-08-05 03:15:28 +0900

    Chore: user model 아이디 부분 수정

--- server/src/models/user.model.ts
+++ server/src/models/user.model.ts
@@ -2,16 +2,19 @@ import { Sequelize, DataType, Model } from 'sequelize-typescript';
 import { sequelize } from '.';
 
 export interface UserAttributes {
+	id?: number;
 	name: string;
 	githubID?: string;
+	refresh_token?: string;
 	createdAt?: Date;
 	updatedAt?: Date;
 }
 
 export default class User extends Model<UserAttributes> {
 	public readonly id!: number;
-	public githubID?: string;
 	public name!: string;
+	public githubID?: string;
+	public refresh_token?: string;
 
 	public createdAt!: Date;
 	public updatedAt!: Date;
@@ -19,13 +22,17 @@ export default class User extends Model<UserAttributes> {
 
 User.init(
 	{
+		name: {
+			type: DataType.STRING(45),
+			allowNull: false,
+		},
 		githubID: {
 			type: DataType.STRING(45),
 			allowNull: true,
 		},
-		name: {
-			type: DataType.STRING(45),
-			allowNull: false,
+		refresh_token: {
+			type: DataType.STRING(200),
+			allowNull: true,
 		},
 		createdAt: {
 			type: 'TIMESTAMP',
