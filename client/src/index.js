6b51266b9c100f8940754a8e91b947fe6c394e21
jjunyjjuny <jjuny546@gmail.com>
2021-08-05 15:52:15 +0900

    Feat: 새고로침시 재로그인 요청

--- client/src/index.js
+++ client/src/index.js
@@ -1,21 +1,25 @@
-import './style.scss';
 import App from './app';
+import './style.scss';
+
 import {
 	createElement,
 	excuteDidMountCallbackQueue,
 } from './core/createElement';
+import { requestSilentRefresh } from './utils/request';
 
-const $test = document.getElementById('test');
+// const $test = document.getElementById('test');
 
-$test.addEventListener('click', async () => {
-	const res = await fetch('http://localhost:4000/api/record?userID=1', {
-		credentials: 'include',
-	}).then((res) => {
-		if (!res.ok) return;
-		return res.json();
-	});
-});
+// $test.addEventListener('click', async () => {
+// 	const res = await fetch('http://localhost:4000/api/record?userID=1', {
+// 		credentials: 'include',
+// 	}).then((res) => {
+// 		if (!res.ok) return;
+// 		return res.json();
+// 	});
+// });
 
 const $app = document.getElementById('app');
 $app.append(createElement(App));
 excuteDidMountCallbackQueue();
+
+window.addEventListener('DOMContentLoaded', requestSilentRefresh);
