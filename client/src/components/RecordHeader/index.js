e5cb0208318f8367de5fb00ab1ba28287c84ac38
negu63 <dnjsdn2019@gmail.com>
2021-08-05 09:50:37 +0900

    Feature: 레코드 헤더 state 추가

--- client/src/components/RecordHeader/index.js
+++ client/src/components/RecordHeader/index.js
@@ -1,29 +1,37 @@
 import html from '../../core/jsx';
 import { createElement } from '../../core/createElement';
-import { appState } from '../../sample/counterVM';
+import { recordState } from '../../vm/recordVM';
+import { useState } from '../../core/vm';
 import ImageButton from '../ImageButton';
+import setMoneyFormat from '../../utils/setMoneyFormat';
+
 import './style.scss';
 
 export default function RecordHeader() {
-	const key = appState;
+	const key = recordState;
 
 	const render = () => {
+		const [state, setState] = useState(key);
+
+		const { records, totalIn, totalOut } = state;
 		return html`<div class="record-header">
-			<div class="record-title">전체 내역 13건</div>
+			<div class="record-title">전체 내역 ${records.length}건</div>
 			<div class="record-io">
 				<div class="record-in">
 					${createElement(ImageButton, {
 						class: '',
 						name: 'save-button-small-active',
+						eventHandler: () => {},
 					})}
-					<span>수입 1,822,480</span>
+					<span>수입 ${setMoneyFormat(totalIn)}</span>
 				</div>
 				<div class="record-out">
 					${createElement(ImageButton, {
 						class: '',
 						name: 'save-button-small-active',
+						eventHandler: () => {},
 					})}
-					<span>지출 798,180</span>
+					<span>지출 ${setMoneyFormat(totalOut)}</span>
 				</div>
 			</div>
 		</div>`;
